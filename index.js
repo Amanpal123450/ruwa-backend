679020e6d2501aaf0edcfd0abd4040226f60bc99
Amanpal123450 <amanpal600@gmail.com>
2025-09-06 16:08:37 +0530

    tushar

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const PORT = process.env.PORT || 8000;
 
 
 
-const allowedOrigins = ["http://localhost:3000","http://localhost:3001","https://frontend-five-iota-49.vercel.app","https://ruwa-india-admin.vercel.app","https://ruwa-user-employee-frontend.vercel.app"];
+const allowedOrigins = ["http://localhost:3000","http://localhost:3001","https://ruwa-india-admin.vercel.app","https://ruwa-user-employee-frontend.vercel.app"];
 
 app.use(cors({
   origin: allowedOrigins,
