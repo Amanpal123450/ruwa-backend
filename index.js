b0e71f4ac065a6cad0690c126d6c33ce070c26a3
Tushar Sharma <tusharsharma0084@gmail.com>
2025-09-26 17:55:06 +0530

    tushar

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const io = new Server(server, {
 
 
 app.use(cors({
-  origin: ["http://localhost:3000","http://localhost:3001","https://ruwa-india-admin.vercel.app","https://ruwa-user-employee-frontend.vercel.app"],
+  origin: ["http://localhost:3000","http://localhost:3001","https://ruwaindia.in","https://ruwa-india-admin.vercel.app","https://ruwa-user-employee-frontend.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
